9de03381c0211086c9fc49cbcd0c2b405bd115a8
ThaminduR <thamindu.17@cse.mrt.ac.lk>
2020-02-11 14:31:50 +0530

    routing changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var userRouter = require('./users');
+
+
+router.use('/user',userRouter)
 
 module.exports = router;
