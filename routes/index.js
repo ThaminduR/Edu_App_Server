1c71c1243a3d46d36e3a176b82727e2bd2d324fe
ThaminduR <thamindu.17@cse.mrt.ac.lk>
2020-02-11 16:12:04 +0530

    CICD

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
+//Routers
 var userRouter = require('./users');
-
-
+//Routes
 router.use('/user',userRouter)
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var admin = require('firebase-admin');
+
+
+//Firebase Admin
+var admin = require("firebase-admin");
+var serviceAccount = require("../grade-5-4016d-firebase-adminsdk-v1i3i-09e7b76cb1.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://grade-5-4016d.firebaseio.com"
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res) {
+  var db = admin.firestore();
+  var ref = db.collection("users")
+  ref.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, ":", doc.data());
+      });
+    })
   res.send('respond with a resource');
 });
 
